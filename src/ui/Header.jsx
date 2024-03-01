4eb3fa7a7cdf5b41d6d42c2c429b1d4db9efc78f
nikhil <nikhilmara123@gmail.com>
2024-03-01 15:02:04 +0530

    updated header

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { useState } from "react";
 
 import HeaderDiv from "./HeaderDiv";
 import Logo from "./Logo";
@@ -9,6 +10,7 @@ const HeaderContainer = styled.header`
   width: 100%;
   background-color: white;
   display: flex;
+  align-items: center;
   border-bottom: 1px solid var(--color-grey-100);
 `;
 
@@ -18,24 +20,62 @@ const StyledInput = styled.input`
   border-radius: var(--border-radius-xl);
   padding: 0.8rem 1.2rem;
   box-shadow: var(--shadow-sm);
-  min-width: 80%;
+  width: 100%;
 
   &:focus {
     outline: none;
     border-color: var(--color-grey-300);
     box-shadow: var(--shadow-sm);
     background-color: var(--color-grey-0);
+    height: 90%;
+    align-self: flex-end;
+    border-bottom: none;
+    border-bottom-left-radius: 0%;
+    border-bottom-right-radius: 0%;
   }
 `;
 
+const Container = styled.div`
+  position: absolute;
+  top: calc(100%);
+  left: 0;
+  width: 100%;
+  height: 200px;
+  /* background-color: red; */
+  border: 1px solid red;
+  border-top: none;
+  border-bottom-left-radius: var(--border-radius-xl);
+  border-bottom-right-radius: var(--border-radius-xl);
+  z-index: 1;
+`;
+
+const SearchBarDiv = styled.div`
+  position: relative;
+  width: 80%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
 const Header = () => {
+  const [isFocused, setIsFocused] = useState(false);
+
   return (
     <HeaderContainer>
       <HeaderDiv>
         <Logo />
       </HeaderDiv>
       <HeaderDiv>
-        <StyledInput type="searchbar" placeholder="Search for your assembly" />
+        <SearchBarDiv>
+          <StyledInput
+            type="searchbar"
+            placeholder="Search for your assembly"
+            onFocus={() => setIsFocused(true)}
+            onBlur={() => setIsFocused(false)}
+          />
+          {isFocused && <Container />}
+        </SearchBarDiv>
       </HeaderDiv>
       <HeaderDiv>
         <Button variation="primary">Login</Button>
